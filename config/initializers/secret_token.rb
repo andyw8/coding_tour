3f664e0063484dd5afb62e71248a7820fcb6873b
Andy Waite <github.aw@andywaite.com>
2013-12-02 17:55:47 -0600

    Read secret_key_base from environment in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,5 @@
-# Be sure to restart your server when you modify this file.
+if Rails.env.production? && ENV['SECRET_KEY_BASE'].blank?
+  raise 'SECRET_KEY_BASE variable must be set!'
+end
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-CodingTour::Application.config.secret_key_base = '042eed5c845cac80f13ed135d143bd14e193273df0a1674d400cbebc5afbe364982ac6874157b09126664531b8bbd6ffb44dbc36bf25d4a62b45344caba1ffa7'
+CodingTour::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || '042eed5c845cac80f13ed135d143bd14e193273df0a1674d400cbebc5afbe364982ac6874157b09126664531b8bbd6ffb44dbc36bf25d4a62b45344caba1ffa7'
