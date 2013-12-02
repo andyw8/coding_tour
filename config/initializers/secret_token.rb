bb493a9a19cd7b8ba0886b6f4a912b12df3ac3ef
Andy Waite <github.aw@andywaite.com>
2013-12-02 16:49:00 -0600

    New app (Rails 4.0.1, Ruby 2.0.0)
    .railsrc settings:
    
    --database=postgresql
    --skip-test-unit
    --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodingTour::Application.config.secret_key_base = '042eed5c845cac80f13ed135d143bd14e193273df0a1674d400cbebc5afbe364982ac6874157b09126664531b8bbd6ffb44dbc36bf25d4a62b45344caba1ffa7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodingTour::Application.config.session_store :cookie_store, key: '_coding_tour_session'
