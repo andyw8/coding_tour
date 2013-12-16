bd44fb4bf2cd2178af0097590fa18c673e028dbf
Andy Waite <github.aw@andywaite.com>
2013-12-16 14:34:50 -0800

    Don't check for Airbrake key in dev or test

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,6 @@
-raise "AIRBRAKE_API_KEY must be set" if ENV['AIRBRAKE_API_KEY'].blank?
+unless Rails.env.development? || Rails.env.test?
+  raise "AIRBRAKE_API_KEY must be set" if ENV['AIRBRAKE_API_KEY'].blank?
+end
 
 Airbrake.configure do |config|
   config.api_key = ENV['AIRBRAKE_API_KEY']
